64525a1e7ebbe3fc898d69266bfc3b63febb9f24
Probal Ghosh <probalghoshofficial@gmail.com>
2025-10-26 15:46:25 +0530

    vercel insights added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Home from "./App/Home"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const App = () => {
   return (
 <div className="min-h-screen w-full bg-white dark:bg-black relative">
+  <SpeedInsights/>
   <BrowserRouter>
   <Routes>
       <Route path="/" element={<Home/>}/>
